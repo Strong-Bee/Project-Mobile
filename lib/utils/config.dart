9759fc8016e7cbec35763cea3c6368522d67194d
Stroong-Bee <lintangsyahdewo26@gmail.com>
2025-11-12 15:57:37 +0700

    first commit

--- lib/utils/config.dart
+++
@@ -1,6 +0,0 @@
-class Config {
-  static const String appName = "Coach Aldo Ranch Absensi";
-  static const String apiUrl = "https://api.coachaldoranch.com";
-  static const String primaryColor = "#FF6B35";
-  static const String secondaryColor = "#8B4513";
-}
